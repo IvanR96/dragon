f9407b8b5f71e8f763c5613aade74976d45e94e9
IvanR96 <irios96@hotmail.com>
2024-01-30 21:10:13 -0800

    main game logic

--- src/index.js
+++ src/index.js
@@ -44,27 +44,23 @@ const game = () =>{
     button3.onclick = fightDragon();
 
     function update(location){
-
-    }
-
-    function goTown(){
         button1.innerText = "Go to store";
         button2.innerText = "Go to cave";
-        button3.innerText = "Fight dragon";
+         button3.innerText = "Fight dragon";
         button1.onclick = goStore();
         button2.onclick = goCave();
         button3.onclick = fightDragon();
         text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+
+    }
+
+    function goTown(){
+        update();
+        
     }
 
     function goStore(){
-        button1.onclick.innerText = 'Buy 10 health (10 gold)';
-        button2.onclick.innerText = "Buy weapon (30 gold)";
-        button3.onclick.innerText = "Go to town square";
-        button1.onclick = buyHealth();
-        button2.onclick = buyWeapon();
-        button3.onclick = goTown();
-        text.innerText = 'You enter the store.';
+        
     }
 
 
